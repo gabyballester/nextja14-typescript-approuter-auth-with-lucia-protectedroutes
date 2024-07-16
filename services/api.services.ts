2124240714a201abe7d204cb753b9b0b84ecd408
gabyballester <gaby.ballester@gmail.com>
2024-07-16 13:48:59 +0200

    Add api services

--- services/api.services.ts
+++ services/api.services.ts
@@ -1,57 +1,7 @@
-import { TrainingCount } from "@/types";
-import sql from "better-sqlite3";
+import { Training } from "@/types";
+import db from "./db.services";
 
-const db = sql("training.db");
-
-db.exec(`
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    email TEXT UNIQUE,
-    password TEXT
-  );
-`);
-
-db.exec(`CREATE TABLE IF NOT EXISTS sessions (
-  id TEXT NOT NULL PRIMARY KEY,
-  expires_at INTEGER NOT NULL,
-  user_id TEXT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-)`);
-
-db.exec(`
-  CREATE TABLE IF NOT EXISTS trainings (
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    image TEXT,
-    description TEXT
-  );
-`);
-
-// Preparamos la consulta
-const statement = db.prepare<TrainingCount>(
-  "SELECT COUNT(*) as count FROM trainings"
-);
-
-// Ejecutamos la consulta sin parámetros
-const result = statement.get({
-  count: 0
-}) as TrainingCount;
-
-// Verificamos si hay entrenamientos
-const hasTrainings = result.count > 0;
-
-if (!hasTrainings) {
-  db.exec(`
-    INSERT INTO trainings (title, image, description)
-    VALUES
-    ('Yoga', '/yoga.jpg', 'A gentle way to improve flexibility and balance.'),
-    ('Boxing', '/boxing.jpg', 'A high-energy workout that improves strength and speed.'),
-    ('Running', '/running.jpg', 'A great way to improve cardiovascular health and endurance.'),
-    ('Weightlifting', '/weightlifting.jpg', 'A strength-building workout that helps tone muscles.'),
-    ('Cycling', '/cycling.jpg', 'A low-impact workout that improves cardiovascular health and endurance.'),
-    ('Gaming', '/gaming.jpg', 'A fun way to improve hand-eye coordination and reflexes.'),
-    ('Sailing', '/sailing.jpg', 'A relaxing way to enjoy the outdoors and improve balance.');
-`);
-}
-
-export default db;
+export const getTrainings = (): Training[] => {
+  const stmt = db.prepare("SELECT * FROM trainings");
+  return stmt.all() as Training[];
+};
