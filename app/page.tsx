fbe34d5a961fd36eb3e8ad9070060ab7682146f0
gabyballester <gaby.ballester@gmail.com>
2024-07-16 13:22:16 +0200

    Add auth form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { AuthForm } from "@/components";
+
 export default function Home() {
-  return <h1>Auth</h1>;
+  return <AuthForm />;
 }
---
+++ components/auth-form.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+
+export const AuthForm = () => {
+  return (
+    <form id="auth-form">
+      <div>
+        <img src="/images/auth-icon.jpg" alt="A lock icon" />
+      </div>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </p>
+      <p>
+        <button type="submit">Create Account</button>
+      </p>
+      <p>
+        <Link href="/">Login with existing account.</Link>
+      </p>
+    </form>
+  );
+};
